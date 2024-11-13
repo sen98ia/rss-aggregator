6a57bbbb371984a26e680023905ce4d9682196fa
sen98ia <sen98ia@gmail.com>
2024-11-13 19:21:01 +0300

    fix eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,22 @@
-import globals from "globals";
+import globals from 'globals';
 
-import path from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-import pluginJs from "@eslint/js";
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import pluginJs from '@eslint/js';
 
 // mimic CommonJS variables -- not needed if using CommonJS
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended});
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: pluginJs.configs.recommended,
+});
 
 export default [
-  ...compat.extends("airbnb-base"),
+  ...compat.extends('airbnb-base'),
   {
-    languageOptions: { 
+    languageOptions: {
       globals: globals.browser,
       parserOptions: {
         ecmaVersion: 'latest',
@@ -40,5 +43,5 @@ export default [
       'no-console': 'off',
       'import/no-extraneous-dependencies': 'off',
     },
-  }
+  },
 ];

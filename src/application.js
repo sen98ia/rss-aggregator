8d86c1a9eb93a6a94694a45c6a6f7b93912b6476
sen98ia <sen98ia@gmail.com>
2024-12-07 14:57:41 +0300

    update src/

---
+++ src/application.js
@@ -0,0 +1,23 @@
+// import * as yup from 'yup';
+
+// const schema = yup.string().url().nullable();
+
+export default () => {
+  // const form = document.querySelector('.rss-form');
+  // const input = document.getElementById('url-input');
+  // const submitButton = document.querySelector('button[type="submit"]');
+  // console.log(submitButton);
+
+  // const initialState = {
+  //   form: {
+  //     urlField: '',
+  //   },
+  //   validationProcess: {
+  //     formState: 'filling',
+  //     validationState: true,
+  //     errors: [], // может объект
+  //   },
+  //   feedList: [],
+  // };
+  console.log('Hello, world!');
+};

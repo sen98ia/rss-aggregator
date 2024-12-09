8696821781594c455190e0a2d8b3153e6c845407
sen98ia <sen98ia@gmail.com>
2024-12-09 21:08:05 +0300

    update src/application.js

--- src/application.js
+++ src/application.js
@@ -1,23 +1,47 @@
-// import * as yup from 'yup';
+import * as yup from 'yup';
+import onChange from 'on-change';
+import render from './view.js';
 
-// const schema = yup.string().url().nullable();
+const validate = (url, urls) => {
+  const schema = yup.string().url().nullable().notOneOf(urls);
+  return schema.validate(url)
+    .then(() => null)
+    .catch((error) => error.message);
+};
 
 export default () => {
-  // const form = document.querySelector('.rss-form');
-  // const input = document.getElementById('url-input');
+  const form = document.querySelector('.rss-form');
+  const input = document.getElementById('url-input');
   // const submitButton = document.querySelector('button[type="submit"]');
-  // console.log(submitButton);
+  console.log(input);
 
-  // const initialState = {
-  //   form: {
-  //     urlField: '',
-  //   },
-  //   validationProcess: {
-  //     formState: 'filling',
-  //     validationState: true,
-  //     errors: [], // может объект
-  //   },
-  //   feedList: [],
-  // };
-  console.log('Hello, world!');
+  const initialState = {
+    form: {
+      valid: true,
+      formState: 'filling', // submitted
+      inputValue: '',
+      error: '',
+    },
+    feedList: [],
+  };
+
+  const watchedState = onChange(initialState, render(input, initialState));
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const inputData = formData.get('url');
+    validate(inputData, watchedState.feedList)
+      .then((error) => {
+        if (error) {
+          watchedState.form.valid = false;
+          watchedState.form.error = error;
+        } else {
+          watchedState.form.valid = true;
+          watchedState.form.error = '';
+          watchedState.feedList.push(inputData);
+        }
+      });
+  });
 };

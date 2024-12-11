713b04d2ba849bc1d405dca21a30c2fdddb9efae
sen98ia <sen98ia@gmail.com>
2024-12-11 22:34:52 +0300

    add ru lang

---
+++ src/locales/ru.js
@@ -0,0 +1,10 @@
+export default {
+  ru: {
+    translation: {
+      invalidUrl: 'Ссылка должна быть валидным URL',
+      duplicatedUrl: 'RSS уже существует',
+      emptyInput: 'Не должно быть пустым',
+      successfulValidation: 'RSS успешно загружен',
+    },
+  },
+};

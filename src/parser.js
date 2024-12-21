f6a45a25f4c36f97be3241af3bc13226049ac3ba
sen98ia <sen98ia@gmail.com>
2024-12-21 21:07:41 +0300

    update parser

--- src/parser.js
+++ src/parser.js
@@ -1,9 +1,44 @@
-export default (data, type, parser) => {
+const getFeedContent = (doc) => {
+  const title = doc.querySelector('title');
+  const description = doc.querySelector('description');
+
+  return { title: title.textContent, description: description.textContent };
+};
+
+const getPostsContent = (doc) => {
+  const titles = doc.querySelectorAll('item > title');
+  const links = doc.querySelectorAll('item > link');
+  const descriptions = doc.querySelectorAll('item > description');
+
+  const titlesArray = Array.from(titles);
+  const linksArray = Array.from(links);
+  const descriptionsArray = Array.from(descriptions);
+
+  const postsContentArray = titlesArray.map((title, i) => {
+    const content = {
+      title: title.textContent,
+      link: linksArray[i].textContent,
+      description: descriptionsArray[i].textContent,
+    };
+    return content;
+  });
+
+  return postsContentArray;
+};
+
+export default (data, type, contentType, parser = new DOMParser()) => {
   const parsedData = parser.parseFromString(data, type);
   const errorElement = parsedData.querySelector('parsererror');
   if (errorElement) {
     throw new Error('invalidRSS');
   } else {
-    return parsedData;
+    switch (contentType) {
+      case ('feedContent'):
+        return getFeedContent(parsedData);
+      case ('postsContent'):
+        return getPostsContent(parsedData);
+      default:
+        throw new Error('unknown content type');
+    }
   }
 };
